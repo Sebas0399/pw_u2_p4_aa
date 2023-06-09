cc599518100b3403200b5e3081ffa9535b680f03
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-08 19:43:24 -0500

    Vue V-on

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 console.log("Componente VUE");
 console.log(Vue);
+//Option API
 const app = Vue.createApp({
-  template: `
-   <h1>Hola Mundo</h1>
-   <p>Desde Vue.js</p>
-   `,
+  //template: `
+  //<h1>Hola Mundo</h1>
+  //<p>Desde Vue.js</p>
+  //<p>{{1+1}}</p>
+  //`,
+  data() {
+    return {
+      nombre:"Antony",
+      apellido:"Arguello",
+      mensaje:"Hola Mundo 2 desde Vue.js"
+    };
+  },
+  methods: {
+    cambiarNombre(){
+      console.log("boton cambiar nombre")
+      this.nombre="Sebastian"
+    },
+    cambiarApellido(){
+      console.log("boton cambiar apellido")
+      this.apellido="Aguay"
+    }
+  },
 });
 app.mount("#miApp");
