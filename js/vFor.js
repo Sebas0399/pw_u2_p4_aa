0bf391d6eba2801448a55e45b18550e6d0c3da49
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-10 10:47:58 -0500

    Vue V-for

--- js/vFor.js
+++ js/vFor.js
@@ -1,5 +1,46 @@
+var miArreglo = [
+  { nombre: "Antony", apellido: "Arguello" },
+  { nombre: "Sebastian", apellido: "Aguay" },
+  { nombre: "Josue", apellido: "Masabanda" },
+  { nombre: "David", apellido: "Chancusi" },
+  { nombre: "Domenica", apellido: "Vizcarra" },
+  { nombre: "Tamara", apellido: "Cabrera" },
+  { nombre: "Luis", apellido: "Mosquera" },
+  { nombre: "Feranando", apellido: "Romero" },
+  { nombre: "Juan", apellido: "Jiminez" },
+  { nombre: "Ignacio", apellido: "Yaguari" },
+];
+console.log(miArreglo);
+
 const app = Vue.createApp({
+  data() {
+    return {
+      miArreglo: miArreglo,
+      nombre: "",
+    };
+  },
+  methods: {
+    agregarEstudiante(event) {
+      if (event.charCode == "13") {
+        const nuevoEstudiate={
+          nombre:this.nombre,
+          apellido:'nuevoApellido'
+        }
+        this.miArreglo.unshift(nuevoEstudiate)
+        console.log(event)
+      } else {
+        console.log("no es enter");
+      }
+    },
+    agregarEstudiante2() {
    
-  });
-  app.mount("#miApp2");
-  
+        const nuevoEstudiate={
+          nombre:this.nombre,
+          apellido:'nuevoApellido'
+        }
+        this.miArreglo.unshift(nuevoEstudiate)
+       
+    }
+  },
+});
+app.mount("#miApp2");
